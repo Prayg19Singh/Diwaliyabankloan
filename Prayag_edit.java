b14b938b9fb6805acf3e2ce407bb1b1304d3b352
Prayag <prayagsingh19bbau@gmail.com>
2023-12-31 23:21:13 +0530

    2nd file modified by prayag

--- Prayag_edit.java
+++ Prayag_edit.java
@@ -1,4 +1,6 @@
 Hii everyone I am creating a new java file on last day of year
 
 is baar kuch achha message likh 
-neupane ke naam
+
+
+git conflit ko understand karo properly when two developers working on different files
